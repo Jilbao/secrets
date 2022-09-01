5d9a90af5a444c1bef181a6c280e4db8ae2eca0c
Çağın Yarkın Sarı <cyarkinsari@gmail.com>
2022-09-01 11:02:18 +0300

    init

---
+++ app.js
@@ -0,0 +1,102 @@
+//Boilerplate
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+
+//DB
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+const secret = process.env.DB_SECRET;
+userSchema.plugin(encrypt, { secret: secret, encryptedFields:["password"]});
+
+const UserModel = mongoose.model("user", userSchema);
+
+
+
+//Routes
+app.get("/", (req, res)=>{
+    res.render("home");
+});
+
+app.get("/login", (req, res)=>{
+    res.render("login");
+});
+
+app.get("/register", (req, res)=>{
+    res.render("register");
+});
+
+//Register
+app.post("/register", (req, res)=>{
+    const newUser = new UserModel({
+        email: req.body.username,
+        password: req.body.password
+    });
+    //Check username exist
+    UserModel.findOne({email: req.body.username}, (err, foundUser)=>{
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                res.send("Username already exist!");
+            } else {
+                // Save user to database
+                newUser.save((err)=>{
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        res.render("secrets");
+                    }
+                });
+            }
+        }
+    });
+
+});
+
+//Login
+app.post("/login", (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    UserModel.findOne({email: username}, (err, foundUser)=>{
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser){
+                if (foundUser.password === password){
+                    res.render("secrets");
+                }else{
+
+                }
+            }
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+//Listen
+app.listen(3000, ()=>{
+    console.log("Server is up! Port: 3000");
+});
