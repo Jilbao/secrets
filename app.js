a4cc19dfbbe79c2ba7d6709a9525904e792add64
Çağın Yarkın Sarı <cyarkinsari@gmail.com>
2022-09-01 12:24:01 +0300

    bcrypt register&login

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 app.set("view engine", "ejs");
@@ -39,47 +39,57 @@ app.get("/register", (req, res)=>{
 
 //Register
 app.post("/register", (req, res)=>{
-    const newUser = new UserModel({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    //Check username exist
-    UserModel.findOne({email: req.body.username}, (err, foundUser)=>{
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUser) {
-                res.send("Username already exist!");
+    
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new UserModel({
+            email: req.body.username,
+            password: hash
+        });
+        //Check username exist
+        UserModel.findOne({email: req.body.username}, (err, foundUser)=>{
+            if (err) {
+                console.log(err);
             } else {
-                // Save user to database
-                newUser.save((err)=>{
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        res.render("secrets");
-                    }
-                });
+                if (foundUser) {
+                    res.send("Username already exist!");
+                } else {
+                    // Save user to database
+                    newUser.save((err)=>{
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            res.render("secrets");
+                        }
+                    });
+                }
             }
-        }
+        });
     });
+
+
 
 });
 
 //Login
 app.post("/login", (req, res)=>{
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     UserModel.findOne({email: username}, (err, foundUser)=>{
         if (err) {
             console.log(err);
         } else {
             if (foundUser){
-                if (foundUser.password === password){
-                    res.render("secrets");
-                }else{
-                    res.send("username or password is wrong!")
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result === true) {
+                        res.render("secrets");
+                    } else {
+                        res.send("username or password is wrong!");
+                    }
+                });              
+            }else{
+                res.send("username or password is wrong!");
             }
         }
     });
